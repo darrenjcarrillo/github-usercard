f5cf51b3e1602cb304ec2922b0f606dc69de1b08
Darren J Carrillo <darrenjcarrillo@gmail.com>
2019-09-05 15:03:01 -0700

    Almost done. Having some undefined stuff on number 5

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,18 +3,80 @@
            https://api.github.com/users/<your name>
 */
 
-axios.get("https://api.github.com/users/darrenjcarrillo").then(response => {
-  console.log(response);
-  response.data.forEach(item => {});
-});
-
-const entryPoint = document.querySelector(".cards");
-
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
    Skip to Step 3.
 */
+
+function GithubCard(
+  imgSrc,
+  name,
+  username,
+  location,
+  url,
+  followerCount,
+  followingCount,
+  userBio
+) {
+  //Create Element
+  newCard = document.createElement("div");
+  newImg = document.createElement("img");
+  newDiv = document.createElement("div");
+  newH3 = document.createElement("h3");
+  pUser = document.createElement("p");
+  pLocation = document.createElement("p");
+  pProfile = document.createElement("p");
+  pFollowers = document.createElement("p");
+  pFollowing = document.createElement("p");
+  pBio = document.createElement("p");
+
+  // Class
+  newCard.classList.add("card");
+  newDiv.classList.add("card-info");
+  newH3.classList.add("name");
+  pUser.classList.add("username");
+
+  // Append
+  newCard.appendChild(newImg);
+  newCard.appendChild(newDiv);
+  newDiv.appendChild(newH3);
+  newDiv.appendChild(pUser);
+  newDiv.appendChild(pLocation);
+  newDiv.appendChild(pProfile);
+  newDiv.appendChild(pFollowers);
+  newDiv.appendChild(pFollowing);
+  newDiv.appendChild(pBio);
+
+  // Content
+  newImg.src = imgSrc;
+  newH3.textContent = `Name: ${name}`;
+  pUser.textContent = username;
+  pLocation.textContent = `Location: ${location}`;
+  pProfile.textContent = `Profile: ${url}`;
+  pFollowers.textContent = `Followers: ${followerCount}`;
+  pFollowing.textContent = `Following: ${followingCount}`;
+  pBio.textContent = `Bio: ${userBio}`;
+
+  return newCard;
+}
+
+axios.get("https://api.github.com/users/darrenjcarrillo").then(response => {
+  // console.log(response);
+  const info = response.data;
+  const newGithubCard = GithubCard(
+    info.avatar_url,
+    info.name,
+    info.login,
+    info.location,
+    info.html_url,
+    info.followers,
+    info.following,
+    info.bio
+  );
+
+  document.querySelector(".cards").appendChild(newGithubCard);
+});
 
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
@@ -30,7 +92,37 @@ const entryPoint = document.querySelector(".cards");
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// const followersArray = [
+//   "tetondan",
+//   "dustinmyers",
+//   "justsml",
+//   "luishrd",
+//   "bigknell"
+// ];
+
+axios
+  .get("https://api.github.com/users/darrenjcarrillo/followers")
+  .then(response => {
+    console.log(response);
+    response.data.forEach(item => {
+      GithubCard(item);
+      const followersGithub = GithubCard(
+        item.avatar_url,
+        item.name,
+        item.login,
+        item.location,
+        item.html_url,
+        item.followers_url,
+        item.following,
+        item.bio
+      );
+      document.querySelector(".cards").appendChild(followersGithub);
+    });
+  });
+
+// .catch(error => {
+//   console.log('The Data was not returned', error);
+// })
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -59,41 +151,3 @@ const followersArray = [];
   luishrd
   bigknell
 */
-
-function GithubCard(gitInfo) {
-  //Create Element
-  newCard = document.createElement("div");
-  newImg = document.createElement("img");
-  newDiv = document.createElement("div");
-  newH3 = document.createElement("h3");
-  pUser = document.createElement("p");
-  pLocation = document.createElement("p");
-  pProfile = document.createElement("p");
-  linkP = document.createElement("a");
-  pFollowers = document.createElement("p");
-  pFollowing = document.createElement("p");
-  pBio = document.createElement("p");
-
-  // Class
-  newCard.classList.add("card");
-  newDiv.classList.add("card-info");
-  newH3.classList.add("name");
-  pUser.classList.add("username");
-
-  // Append
-  newCard.appendChild(newImg);
-  newCard.appendChild(newDiv);
-  newDiv.appendChild(newH3);
-  newDiv.appendChild(pUser);
-  newDiv.appendChild(pLocation);
-  newDiv.appendChild(pProfile);
-  newDiv.appendChild(pFollowers);
-  newDiv.appendChild(pFollowing);
-  newDiv.appendChild(pBio);
-  pProfile.appendChild(linkP);
-
-  // Content
-  newImg.src = imgUrl;
-
-  return newCard;
-}
